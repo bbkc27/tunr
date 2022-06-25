f1d33af62845c934783867a6ff7f0380495f0767
bbkc27 <briana.j.casey@gmail.com>
2022-06-25 15:19:54 -0400

    DRF complete

--- tunr/urls.py
+++ tunr/urls.py
@@ -2,14 +2,18 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-  path('artists/', views.artist_list, name='artist_list'),
-  path('artists/<int:pk>', views.artist_detail, name='artist_detail'),
-  path('artists/new', views.artist_create, name='artist_create'),
-  path('artists/<int:pk>/edit', views.artist_edit, name='artist_edit'),
-  path('artists/<int:pk>/delete', views.artist_delete, name='artist_delete'),
-  path('songs/', views.song_list, name='song_list'),
-  path('songs/<int:pk>', views.song_detail, name='song_detail'),
-  path('songs/new', views.song_create, name='song_create'),
-  path('songs/<int:pk>/edit', views.song_edit, name='song_edit'),
-  path('songs/<int:pk>/delete', views.song_delete, name='song_delete'),
+  # path('artists/', views.artist_list, name='artist_list'),
+  path('artists/', views.ArtistList.as_view(), name='artist_list'),
+  # path('artists/<int:pk>', views.artist_detail, name='artist_detail'),
+  path('artists/<int:pk>', views.ArtistDetail.as_view(), name='artist_detail'),
+  # path('artists/new', views.artist_create, name='artist_create'),
+  # path('artists/<int:pk>/edit', views.artist_edit, name='artist_edit'),
+  # path('artists/<int:pk>/delete', views.artist_delete, name='artist_delete'),
+  # path('songs/', views.song_list, name='song_list'),
+  path('songs/', views.SongList.as_view(), name='song_list'),
+  # path('songs/<int:pk>', views.song_detail, name='song_detail'),
+  path('songs/<int:pk>', views.SongDetail.as_view(), name='song_detail'),
+  # path('songs/new', views.song_create, name='song_create'),
+  # path('songs/<int:pk>/edit', views.song_edit, name='song_edit'),
+  # path('songs/<int:pk>/delete', views.song_delete, name='song_delete'),
 ]
--- tunr/views.py
+++ tunr/views.py
@@ -1,12 +1,43 @@
 from django.shortcuts import render, redirect
 from .models import Artist, Song
 from .forms import ArtistForm, SongForm
+from django.http import JsonResponse
+from rest_framework import generics
+from .serializers import ArtistSerializer, SongSerializer
 
 # Create your views here.
 
+#drf stuff
+class ArtistList(generics.ListCreateAPIView):
+  queryset = Artist.objects.all()
+  serializer_class = ArtistSerializer
+
+class ArtistDetail(generics.RetrieveUpdateDestroyAPIView):
+  queryset = Artist.objects.all()
+  serializer_class = ArtistSerializer
+
+class SongList(generics.ListCreateAPIView):
+  queryset = Song.objects.all()
+  serializer_class = SongSerializer
+
+class SongDetail(generics.RetrieveUpdateDestroyAPIView):
+  queryset = Song.objects.all()
+  serializer_class = SongSerializer
+
+
+
+#old stuff
 def artist_list(request):
   artists = Artist.objects.all()
-  return render(request, 'tunr/artist_list.html', {'artists': artists})
+  values_artists = artists.values('name', 'nationality', )
+  list_artists = list(values_artists)
+ 
+  # print(type(artists))
+  # print(type(values_artists))
+  # print(type(list_artists))
+
+  return JsonResponse(list_artists, safe=False)
+  # return render(request, 'tunr/artist_list.html', {'artists': artists})
 
 def song_list(request):
   songs = Song.objects.all()
